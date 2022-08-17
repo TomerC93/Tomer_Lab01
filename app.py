5613abf5478b19a25aa6cd7e3adecec14185ac85
TomerC93 <106026622+TomerC93@users.noreply.github.com>
2022-08-17 10:34:36 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import boto3
+ec2 = boto3.resource('ec2')
+
+for instance in ec2.instances.all():
+        kube_machine = False
+        for tag in instance.tags:
+                if tag['Value'] == "1" and tag['Key'] == "k8s.io/role/master" and instance.state["Code"] == 16:
+                    kube_machine = True
+        if kube_machine == True:
+                for tag in instance.tags:
+                    if tag['Key'] == "Name":
+                        print(tag['Value'])
+                        print(instance.id)
+
